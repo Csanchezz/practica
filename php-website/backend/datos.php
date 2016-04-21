b09d95663fe84ac99b2ed66ab3fcf282ec91c93e
Csanchezz <csanchezz@thinkandmake.net>
2016-04-20 21:24:07 -0600

    Agregar array php-website!

---
+++ php-website/backend/datos.php
@@ -0,0 +1,49 @@
+<?php
+$array_users = array(
+	array(
+		'id' => '1',
+		'nombre' => 'Luis Carlos',
+		'apellido' => 'Contreras',
+		'puesto' => 'El que hace el café',
+		'anno_nacimiento' => 1993,
+		'empresa' => 'Funsepa',
+		'telefono'=>array(12345678)
+		),
+	array(
+		'id' => '2',
+		'nombre' => 'Hans',
+		'apellido' => 'Girón',
+		'puesto' => 'El que toma café',
+		'anno_nacimiento' => 1998,
+		'empresa' => 'UMG',
+		'telefono'=>array(12345678)
+		),
+	array(
+		'id' => '3',
+		'nombre' => 'fulanito',
+		'apellido' => 'mengano',
+		'puesto' => 'El que toma te',
+		'anno_nacimiento' => 1980,
+		'empresa' => 'UMG',
+		'telefono'=>array(12345678)
+		),
+	array(
+		'id' => '4',
+		'nombre' => 'Carlos',
+		'apellido' => 'Sanchez',
+		'puesto' => 'Al que no le gusta el café',
+		'anno_nacimiento' => 1999,
+		'empresa' => 'Colegio',
+		'telefono'=>array(12345678)
+		),
+	array(
+		'id' => '5',
+		'nombre' => 'Felipe',
+		'apellido' => 'Del valle',
+		'puesto' => 'El aguador',
+		'anno_nacimiento' => 1995,
+		'empresa' => 'San Carlos',
+		'telefono'=>array(12345678)
+		)
+	);
+?>
